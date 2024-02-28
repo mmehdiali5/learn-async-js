3b061d10916d26df999a8f23ea1b2334bd5358cb
mmehdiali5 <kaka15121472@gmail.com>
2024-02-27 23:43:08 -0500

    neg 2d

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,34 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+const findNegative = (nums) => {
+    let negatives = [];
+    for (const num of nums) {
+        if (num < 0) {
+            negatives.push(num)
+        }
+    }
+    return negatives;
+}
+
+const row1 = (rowOne) => {
+    return findNegative(rowOne);
+}
+
+const row2 = (rowTwo) => {
+    return findNegative(rowTwo);
+}
+
+const row3 = (rowThree) => {
+    return findNegative(rowThree);
+}
+
+
+Promise.all([row1(array2D[0]), row2(array2D[1]), row3(array2D[2])])
+    .then((negatives) => {
+        console.log(negatives)
+    })
+    .catch((error) => {
+        console.error("Unsuccessful Operation");
+    });
